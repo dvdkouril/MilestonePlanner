a713fc30ead3c0b44980d9dc5d903f4b3b0fd8f3
David Kouřil <david.kouril@gmail.com>
2015-06-30 16:44:55 +0200

    composite pattern basic classes

---
+++ MilestonePlanner/MilestoneComponent.swift
@@ -0,0 +1,13 @@
+//
+//  MilestoneComponent.swift
+//  MilestonePlanner
+//
+//  Created by David Kouřil on 30/06/15.
+//  Copyright (c) 2015 dvdkouril. All rights reserved.
+//
+
+import Foundation
+
+class MilestoneComponent {
+    
+}
---
+++ MilestonePlanner/MilestoneLeaf.swift
@@ -0,0 +1,13 @@
+//
+//  MilestoneLeaf.swift
+//  MilestonePlanner
+//
+//  Created by David Kouřil on 30/06/15.
+//  Copyright (c) 2015 dvdkouril. All rights reserved.
+//
+
+import Foundation
+
+class MilestoneLeaf {
+    
+}
