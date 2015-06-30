b2d1f62121ba75db79791d9a4d853baffe8cee87
David Kouřil <david.kouril@gmail.com>
2015-06-30 16:41:47 +0200

    initial commit

---
+++ MilestonePlanner/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MilestonePlanner
+//
+//  Created by David Kouřil on 30/06/15.
+//  Copyright (c) 2015 dvdkouril. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
